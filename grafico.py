ea3a95fbea0bc2d842214a232e70772b69609fdd
Marcelo Alcántara <46867671+Marcee13@users.noreply.github.com>
2025-02-01 10:49:11 -0600

    Carga de archivos

---
+++ grafico.py
@@ -0,0 +1,276 @@
+#Logos obtenidos de: https://www.flaticon.es/
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox, filedialog
+import os
+import recuperar
+import threading
+import subprocess
+import sys
+from PIL import Image, ImageTk
+
+# Ruta por defecto (modificable)
+DEFAULT_PATH = os.path.expanduser("~/Downloads/BackUp_CensoAgropecuario")
+
+# Función para verificar y obtener el número de serie de los dispositivos conectados
+def obtener_numero_serie():
+    try:
+        result = subprocess.Popen(
+            ['adb', 'devices'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            creationflags=subprocess.CREATE_NO_WINDOW # Se evita la creacion de ventana CMD por cada ejecucion de la funcion
+        )
+        output, error = result.communicate()  # Captura la salida del comando
+        output = output.decode('utf-8')
+        
+        lines = output.splitlines()
+        devices = [line.split()[0] for line in lines if line.endswith("\tdevice")]
+
+        if devices:
+            return devices[0]
+        else:
+            return None
+    except FileNotFoundError:
+        print("ADB no está instalado.")
+        return None
+
+# Función para actualizar el mensaje del dispositivo conectado
+def mostrar_info_dispositivo():
+    serial = obtener_numero_serie()
+    if serial:
+        label_info_dispositivo.config(text=f"Dispositivo conectado: {serial}")
+    else:
+        label_info_dispositivo.config(text="No hay dispositivos conectados.")
+    
+    ventana.after(1000, mostrar_info_dispositivo)
+
+# Función que actualiza la barra de progreso
+def update_progress(progress):
+    progress_bar['value'] = progress
+    ventana.update_idletasks()
+
+# Función para actualizar el mensaje en la interfaz
+def update_message(message):
+    label_resultado.config(text=message)
+    ventana.update_idletasks()
+
+# Función para seleccionar y cambiar la ruta de destino
+def cambiar_ruta():
+    carpeta = filedialog.askdirectory(title="Selecciona la carpeta de destino")
+    if carpeta:
+        label_ruta.config(text=f"Ruta de destino: {carpeta}")
+
+# Función que ejecuta el proceso en segundo plano usando hilos
+def ejecutar_script():
+    folder_name = folder_entry.get()
+    if not folder_name:
+        messagebox.showerror("Error", "Debes ingresar el nombre de la carpeta.")
+        return
+
+    ruta_destino = label_ruta.cget("text").replace("Ruta de destino: ", "")
+    if not os.path.exists(ruta_destino):
+        try:
+            # Crear la ruta si no existe
+            os.makedirs(ruta_destino)
+            messagebox.showinfo("Éxito", f"Se creó la ruta: {ruta_destino}")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo crear la ruta. Error: {e}")
+            return
+
+    btn_recuperar.config(state=tk.DISABLED)
+    update_message("Ejecutando proceso...")
+    update_progress(0)
+
+    def hilo_recuperar():
+        try:
+            final_output_folder = os.path.join(ruta_destino, folder_name)
+
+            recuperar.compress_projects_and_pull(final_output_folder, update_callback=update_progress)
+            update_message("Compresión y transferencia completadas...")
+            
+            extracted_folder = recuperar.decompress_projects(final_output_folder, update_callback=update_progress)
+            update_message("Descompresión completada...")
+            
+            recuperar.create_folders_with_project_archive(extracted_folder, final_output_folder, update_callback=update_progress)
+            update_message("Archivos organizados correctamente...")
+            
+            recuperar.clean_up(final_output_folder)
+            update_message("Limpieza completada...")
+
+            ventana.after(0, mostrar_resultado, f"Proceso completado con éxito en: {final_output_folder}")
+        except Exception as e:
+            ventana.after(0, mostrar_error, f"Error: {e}")
+        finally:
+            ventana.after(0, habilitar_boton)
+
+    thread = threading.Thread(target=hilo_recuperar)
+    thread.start()
+
+def mostrar_resultado(resultado):
+    label_resultado.config(text=resultado)
+
+def mostrar_error(mensaje):
+    messagebox.showerror("Error", mensaje)
+
+def habilitar_boton():
+    btn_recuperar.config(state=tk.NORMAL)
+
+def cerrar_ventana():
+    ventana.destroy()
+
+# Detectar si estamos en un archivo .exe
+if getattr(sys, 'frozen', False):
+    # Si estamos en un .exe, obtener la ruta de los archivos extraídos
+    application_path = sys._MEIPASS
+else:
+    # Si estamos ejecutando desde un script, usar la ruta del script
+    application_path = os.path.dirname(os.path.abspath(__file__))
+
+# Ruta completa a las imágenes
+logo_path = os.path.join(application_path, 'logo.png')
+cerrar_path = os.path.join(application_path, 'cerrar.png')
+respaldo_path = os.path.join(application_path, 'respaldo.png')
+carpeta_path = os.path.join(application_path, 'carpeta.png')
+
+# Ventana principal de la aplicación se define el tamaño y otras caracteristicas.
+ventana = tk.Tk()
+ventana.title("Backup y Compresión de Información")
+ventana.geometry("500x400")
+ventana.resizable(False,False)
+
+# Se aplica el logo de la aplicación
+try:
+    icono = tk.PhotoImage(file=logo_path)
+    ventana.iconphoto(False, icono)
+except Exception as e:
+    print(f"Error al cargar el icono: {e}")
+
+# Carga de imagen para los botones
+try:
+    image_salir = Image.open(cerrar_path)
+    image_salir = image_salir.resize((18, 18))
+    iconSalir = ImageTk.PhotoImage(image_salir)
+except Exception as e:
+    print(f"Error al cargar la imagen 'cerrar.png': {e}")
+
+try:
+    image_inicio = Image.open(respaldo_path)
+    image_inicio = image_inicio.resize((18, 18))
+    iconInicio = ImageTk.PhotoImage(image_inicio)
+except Exception as e:
+    print(f"Error al cargar la imagen 'respaldo.png': {e}")
+
+try:
+    image_carpeta = Image.open(carpeta_path)
+    image_carpeta = image_carpeta.resize((18, 18))
+    iconCarpeta = ImageTk.PhotoImage(image_carpeta)
+except Exception as e:
+    print(f"Error al cargar la imagen 'carpeta.png': {e}")
+
+# Etiqueta para mostrar el número de serie del dispositivo
+label_info_dispositivo = tk.Label(
+    ventana,
+    text="Cargando información del dispositivo...",
+    wraplength=450
+)
+label_info_dispositivo.pack(pady=10)
+
+# Funcion para comprobar dispositivos conectados
+mostrar_info_dispositivo()
+
+# Frame para etiqueta y entrada
+frame_folder = tk.Frame(ventana)
+frame_folder.pack(pady=5)
+
+label_folder = tk.Label(
+    frame_folder,
+    text="Nombre de la carpeta:"
+)
+label_folder.pack(side=tk.LEFT, padx=5, pady=20)
+
+folder_entry = tk.Entry(frame_folder, width=40)
+folder_entry.pack(side=tk.LEFT, padx=5)
+
+# Frame para contener la ruta y el botón de cambiar
+frame_ruta = tk.Frame(ventana)
+frame_ruta.pack(pady=10)
+
+# Mostrar la ruta de destino
+label_ruta = tk.Label(
+    frame_ruta,
+    text=f"Ruta de destino: {DEFAULT_PATH}",
+    anchor="w",
+    bg="lightgray",
+    wraplength=350,
+    justify="left"
+)
+label_ruta.pack(side=tk.LEFT, padx=5, fill="x")
+
+# Botón para cambiar la ruta
+btn_cambiar_ruta = tk.Button(
+    frame_ruta,
+    text="Cambiar ruta",
+    image=iconCarpeta,
+    compound="left",
+    padx=5,
+    command=cambiar_ruta
+)
+btn_cambiar_ruta.pack(side=tk.LEFT, padx=5)
+
+# Frame para la barra de progreso y su etiqueta
+frame_progreso = tk.Frame(ventana)
+frame_progreso.pack(pady=5)
+
+label_progress = tk.Label(frame_progreso, text="Progreso:")
+label_progress.pack(side=tk.LEFT, padx=5)
+
+progress_bar = ttk.Progressbar(
+    frame_progreso,
+    length=300,
+    orient='horizontal',
+    mode='determinate',
+    maximum=100
+)
+progress_bar.pack(side=tk.LEFT, padx=5)
+
+# Mostrar los resultados
+label_resultado = tk.Label(
+    ventana,
+    text="Aquí aparecerán los resultados",
+    wraplength=450
+)
+label_resultado.pack(pady=10)
+
+# Frame para los botones
+frame_botones = tk.Frame(ventana)
+frame_botones.pack(pady=20)
+
+btn_recuperar = tk.Button(
+    frame_botones,
+    text="Iniciar Proceso",
+    image=iconInicio,
+    compound="left",
+    anchor="w",
+    padx=10,
+    command=ejecutar_script,
+    width=115
+)
+btn_recuperar.pack(side=tk.LEFT, padx=10)
+
+btn_salir = tk.Button(
+    frame_botones,
+    text="Salir",
+    image=iconSalir,
+    compound="left",
+    anchor="w",
+    padx=10,
+    command=cerrar_ventana,
+    width="70"
+)
+btn_salir.pack(side=tk.LEFT, padx=10)
+
+ventana.mainloop()
+
---
+++ recuperar.py
@@ -0,0 +1,146 @@
+import subprocess
+import os
+import tarfile
+import sqlite3
+import shutil
+
+def obtener_numero_serie():
+    """
+    Obtiene el número de serie del dispositivo Android conectado.
+    Retorna el número de serie si está conectado, o None si no hay dispositivos.
+    """
+    try:
+        devices_output = subprocess.check_output(['adb', 'devices']).decode('utf-8').strip().splitlines()
+        
+        if len(devices_output) > 1:
+            dispositivo_serie = devices_output[1].split()[0]
+            return dispositivo_serie  # Devuelve el número de serie del dispositivo
+        else:
+            return None  # Retorna None si no hay dispositivos conectados
+
+    except subprocess.CalledProcessError as e:
+        return f"Error al obtener el número de serie: {str(e)}"
+        
+def verificar_dispositivo_conectado():
+    try:
+        devices_output = subprocess.check_output(['adb', 'devices']).decode('utf-8').strip().splitlines()
+        return len(devices_output) > 1
+    except subprocess.CalledProcessError:
+        return False
+
+def sanitize_name(name):
+    """Reemplaza los caracteres problemáticos para nombres de archivos."""
+    name = name.replace(":", "_").replace("(", "_").replace(")", "_")
+    return name
+
+def delete_db_journal_files_on_device(update_callback=None):
+    """Elimina los archivos DB-JOURNAL en el dispositivo conectado."""
+    if update_callback:
+        update_callback(10)
+    delete_cmd = "find /storage/emulated/0/Android/data/org.odk.collect.android/files/projects -name '*.db-journal' -exec rm -f {} +"
+    subprocess.run(["adb", "shell", delete_cmd], check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+    if update_callback:
+        update_callback(20)
+
+def compress_projects_and_pull(local_destination_folder, update_callback=None):
+    """Comprime la carpeta 'projects' en el dispositivo y la transfiere a la PC."""
+    os.makedirs(local_destination_folder, exist_ok=True)
+    delete_db_journal_files_on_device()
+
+    remote_tar_path = "/storage/emulated/0/Android/data/org.odk.collect.android/files/projects.tar.gz"
+    compress_cmd = f"toybox tar -czf {remote_tar_path} -C /storage/emulated/0/Android/data/org.odk.collect.android/files projects"
+    subprocess.run(["adb", "shell", compress_cmd], check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+
+    subprocess.run(["adb", "pull", remote_tar_path, local_destination_folder], check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+
+    if update_callback:
+        update_callback(40)
+
+def decompress_projects(local_destination_folder, update_callback=None):
+    """Descomprime el archivo 'projects.tar.gz' en una carpeta local."""
+    tar_gz_path = os.path.join(local_destination_folder, "projects.tar.gz")
+    extracted_folder = os.path.join(local_destination_folder, "projects_extracted")
+    
+    os.makedirs(extracted_folder, exist_ok=True)
+    with tarfile.open(tar_gz_path, "r:gz") as tar_ref:
+        total_files = len(tar_ref.getmembers())
+        for i, member in enumerate(tar_ref.getmembers()):
+            member.name = sanitize_name(member.name)
+            tar_ref.extract(member, path=extracted_folder)
+            
+            # Actualizar el progreso
+            if update_callback:
+                progress = int(((i + 1) / total_files) * 100)
+                update_callback(progress)  # Llamar al callback con el progreso
+            
+    return extracted_folder
+
+
+def create_folders_with_project_archive(extracted_folder, output_folder, update_callback=None):
+    """Organiza los datos extraídos y crea archivos comprimidos en subcarpetas."""
+    projects_root = os.path.join(extracted_folder, "projects")
+    if not os.path.isdir(projects_root):
+        print(f"No existe la carpeta: {projects_root}")
+        return
+
+    subfolders = [f for f in os.listdir(projects_root) if os.path.isdir(os.path.join(projects_root, f))]
+    total_subfolders = len(subfolders)
+    
+    # Recorremos cada subcarpeta
+    for i, folder_name in enumerate(subfolders):
+        subfolder_path = os.path.join(projects_root, folder_name)
+        if not os.path.isdir(subfolder_path):
+            continue
+
+        metadata_dir = os.path.join(subfolder_path, "metadata")
+        if not os.path.isdir(metadata_dir):
+            continue
+        
+        for file_name in os.listdir(metadata_dir):
+            if file_name.lower().endswith(".db"):
+                db_path = os.path.join(metadata_dir, file_name)
+                try:
+                    conn = sqlite3.connect(db_path)
+                    cursor = conn.cursor()
+                    cursor.execute("SELECT DISTINCT codConsultor, idSegmento FROM user_logs;")
+                    rows = cursor.fetchall()
+
+                    os.makedirs(output_folder, exist_ok=True)
+                    for row in rows:
+                        cod_consultor, id_segmento = row
+                        if not id_segmento:
+                            continue
+
+                        segmento_folder = os.path.join(output_folder, str(id_segmento))
+                        os.makedirs(segmento_folder, exist_ok=True)
+                        
+                        consultor_folder = os.path.join(segmento_folder, cod_consultor)
+                        os.makedirs(consultor_folder, exist_ok=True)
+                        
+                        tar_gz_path = os.path.join(consultor_folder, "projects.tar.gz")
+                        with tarfile.open(tar_gz_path, "w:gz") as tar:
+                            tar.add(extracted_folder, arcname="projects")
+
+                    conn.close()
+                except sqlite3.Error as e:
+                    print(f"Error al leer la base de datos '{db_path}': {e}")
+                finally:
+                    conn.close()
+
+        # Actualiza el progreso
+        if update_callback:
+            progress = int(((i + 1) / total_subfolders) * 100)
+            update_callback(progress)        
+
+def clean_up(local_destination_folder):
+    """Elimina los archivos temporales generados durante el proceso."""
+    tar_gz_path = os.path.join(local_destination_folder, "projects.tar.gz")
+    extracted_folder = os.path.join(local_destination_folder, "projects_extracted")
+
+    if os.path.exists(tar_gz_path):
+        os.remove(tar_gz_path)
+    
+    if os.path.exists(extracted_folder):
+        shutil.rmtree(extracted_folder)
+    
+    print("Limpieza completada.")
